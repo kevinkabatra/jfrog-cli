29391de9ba900cdea778e99da97dce80f2ed45f2
Kevin Kabatra <kevinkabatra@gmail.com>
2017-10-16 14:01:54 -0400

    Updated package to support 1.11.2
    Package now points directly to the file versus grabbing the latest version. Grabbing the latest version causes an issue with the Checksum when a newer version is available. Also updated the install script to go along with the suggestions of the chocolatey moderators.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿#JFrog CLI is completely contained within an .exe that is provided by the team at JFrog.
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Get-ChocolateyWebFile `
+    -PackageName 'JFrog CLI' `
+    -FileFullPath "$env:ProgramData\JFrog\jfrog.exe" `
+    -Url 'https://bintray.com/jfrog/jfrog-cli-go/download_file?file_path=1.11.2%2Fjfrog-cli-windows-amd64%2Fjfrog.exe' `
+    -Checksum 'F10555E5145F77A0DB99792CB478B9931E3E053BC648874B8A3E75FEE705709E996417AFAB3494E637041A03C8D1ECA50B73E27C7324562B2AB0F3DB234C0699' `
+    -ChecksumType 'sha512' `
+    -ForceDownload
+    
+Install-ChocolateyPath -PathToInstall "$env:ProgramData\JFrog\" -PathType 'Machine'
+Update-SessionEnvironment
+
+Write-Warning ('jfrog added to PATH. It might be required to restart your session in order to use new the environment variable.')
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,24 @@
+﻿$jfrogDir = "$env:ProgramData\Jfrog"
+if(Test-Path -Path "$jfrogDir\jfrog.exe" -ErrorAction SilentlyContinue)
+{
+    Remove-Item -Path $jfrogDir -Force -Recurse > $null
+}
+else
+{
+    Write-Warning ("Cannot locate jfrog.exe at $jFrogDir.")
+}
+
+$jfrogDir = $jfrogDir -replace '\\','\\' #this formats the path to work with -replace
+$path     = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+$oldPath  = (Get-ItemProperty -Path $path -Name PATH).Path
+if($oldPath | Select-String -Pattern 'jfrog')
+{
+    $newPath  = $oldPath -replace ";$jfrogDir\\",";"
+    Set-ItemProperty -Path $path -Name PATH –Value $newPath
+
+    &"C:\ProgramData\Chocolatey\bin\RefreshEnv.cmd"
+}
+else
+{
+    Write-Warning ('Cannot locate jfrog in PATH variable.')   
+}
