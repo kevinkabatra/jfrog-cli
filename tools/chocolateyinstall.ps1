7ce4a78fe9bf0167bb24208a04c78fa49a6d5105
Mike Hay <mhay@kinaxis.com>
2018-04-27 15:28:28 -0400

    Update to 1.14.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Get-ChocolateyWebFile `
     -PackageName 'JFrog CLI' `
     -FileFullPath "$env:ProgramData\JFrog\jfrog.exe" `
-    -Url 'https://bintray.com/jfrog/jfrog-cli-go/download_file?file_path=1.11.2%2Fjfrog-cli-windows-amd64%2Fjfrog.exe' `
+    -Url 'https://bintray.com/jfrog/jfrog-cli-go/download_file?file_path=1.14.0%2Fjfrog-cli-windows-amd64%2Fjfrog.exe' `
     -Checksum 'F10555E5145F77A0DB99792CB478B9931E3E053BC648874B8A3E75FEE705709E996417AFAB3494E637041A03C8D1ECA50B73E27C7324562B2AB0F3DB234C0699' `
     -ChecksumType 'sha512' `
     -ForceDownload
